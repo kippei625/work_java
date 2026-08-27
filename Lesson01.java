d75e5849d696ea65f7136e89462966ffda90843e
kippei625 <kippei0625@gmail.com>
2026-08-27 22:57:15 +0900

    Lesson01
    一回目

---
+++ Lesson01.java
@@ -0,0 +1,26 @@
+package work_java;
+
+public class Lesson01 {
+
+	public Lesson01() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println("Javaのカリキュラム");
+		
+		
+		
+		System.out.println(1 + 2);
+        System.out.println(100 - 50);
+        System.out.println(50 / 2);
+        System.out.println(10 * 5);
+    }
+
+	
+	
+	
+}
+
