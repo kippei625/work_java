63ae084c8207ec8f17fb3fc40f298ff215290f90
kippei625 <kippei0625@gmail.com>
2026-08-27 23:27:58 +0900

    Lesson02
    1回目

---
+++ Lesson02.java
@@ -0,0 +1,20 @@
+package work_java;
+
+public class Lesson02 {
+
+	public Lesson02() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	public static void main(String[] args) {
+		
+		String name_first;
+		name_first = "きっぺい";
+		String name_family;
+		name_family = "おおかわ";
+		
+		
+		System.out.println(name_family);
+		System.out.println(name_first);
+	}
+
+}
