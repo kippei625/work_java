e984361ef98b0ed87702cdb4ffe1790452881d1d
kippei625 <kippei0625@gmail.com>
2026-08-28 03:05:23 +0900

    Add files via upload

--- Lesson02.java
+++ Lesson02.java
@@ -7,14 +7,33 @@ public class Lesson02 {
 	}
 	public static void main(String[] args) {
 		
-		String name_first;
-		name_first = "きっぺい";
-		String name_family;
-		name_family = "おおかわ";
+		//課題１
 		
+		String name_first,name_family;;
+		name_first = "きっぺい";
+		name_family = "おおかわ";
 		
 		System.out.println(name_family);
 		System.out.println(name_first);
+		
+		//課題２
+		
+		int i,x,y;
+		x = 13;
+		y = 7;
+		
+		int result_1 = x + y;
+		int result_2 = x - y;
+		int result_3 = x * y;
+		double result_4 = (double) x / y; //
+		
+		
+		System.out.println(result_1); //20
+		System.out.println(result_2); //6
+		System.out.println(result_3); //91
+		System.out.println(result_4); //1.85,,,
+		
+		
 	}
 
 }
