49a35c664c550516b155947ce58d625cc27e1c63
kippei625 <kippei0625@gmail.com>
2026-08-28 13:03:28 +0900

    Add files via upload

---
+++ Lesson04.java
@@ -0,0 +1,43 @@
+package work_java;
+
+public class Lesson04 {
+
+	public Lesson04() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
+	public static void main(String[] args) {
+		
+		//課題１
+		
+		/*String name;
+		name = "私の名前は大川輝平で、";
+		String age;
+		age = "年齢は22歳です。";
+		
+		String introduction;
+		introduction = name + age;
+		
+		System.out.println(introduction);
+		*/
+		
+		String name;
+		name = "大川輝平";
+		int age;
+		age = 22;
+		
+		System.out.println("私の名前は" + name + "で、年齢は" + age + "です。");
+		
+		
+		//課題２
+		int x,y,result;
+		x = 4;
+		y = 6;
+		
+		result = x * y;
+		
+		System.out.println("4と6の積は" + result + "です。");
+	
+	}
+
+}
