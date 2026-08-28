f8214894c51c9185e896d746c67e2463de051b6d
kippei625 <kippei0625@gmail.com>
2026-08-28 12:41:19 +0900

    Add files via upload

---
+++ Lesson03.java
@@ -0,0 +1,26 @@
+package work_java;
+
+public class Lesson03 {
+
+	public Lesson03() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
+	public static void main(String[] args) {
+	
+		//課題１
+		String test;
+		
+		//課題２
+		test = "4";
+		
+		//課題３
+		String test_1;
+		test_1 = test;
+		
+		//課題４
+		System.out.println(test_1);
+		
+	}
+	
+}
